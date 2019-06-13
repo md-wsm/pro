5cae434f104c30d44c5e7b085975e78a64d5b49c
huosah <834637578@qq.com>
2019-06-13 11:37:53 +0800

    增加eslint，删除js-cookies，改为session

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	root: true,
+	'extends': [
+		'plugin:vue/essential',
+		'@vue/standard'
+	],
+	rules: {
+		/**
+		  * off 或 0：表示不验证规则。
+		  * warn 或 1：表示验证规则，当不满足时，给警告
+		  * error 或 2 ：表示验证规则，不满足时报错
+		  */
+		// allow async-await
+		'generator-star-spacing': 'off',
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/no-parsing-error': [2, {
+			'x-invalid-end-tag': false
+		}],
+		'camelcase': 'off',
+		'no-var': 2,
+		'no-console': 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['error', 4],
+        'semi': ['error', 'always'],
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
+}
