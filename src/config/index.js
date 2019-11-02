f47d277bb5177c5d5bbcaf3c5cfcf9aa2da40397
xogun <gun@xo.com>
2019-11-02 23:05:05 +0700

    删除部分多余文件/文件夹2

--- src/config/index.js
+++ src/config/index.js
@@ -6,12 +6,7 @@ export default {
     /**
    * @description token在Cookie中存储的天数，默认1天
    */
-    /**
-   * @description 是否使用国际化，默认为false
-   *              如果不使用，则需要在路由中给需要在菜单中展示的路由设置meta: {title: 'xxx'}
-   *              用来在菜单中显示文字
-   */
-    useI18n: false, // 默认这里是true, 直接更改router.js中的title没有效果。只有把这里改为false才行。
+
     /**
    * @description api请求基础路径
    */
